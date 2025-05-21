1ad8a32ed91d836ce36d28418f313194d3430713
McCoffeeee <liam@mccafferty.eu>
2025-05-21 22:02:12 +0100

    Minimal app template pulled

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# thingagotchi
-Tamagotchi-style app for DeskThing
+# Deskthing App Template
+
+Welcome to the DeskThing Template!
+
+For a full wiki on how to make an app, go to https://github.com/itsriprod/deskthing-apps/wiki
--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,49 @@
-import React, { useEffect, useState } from 'react'
-import { DeskThing } from '@deskthing/client'
-import { AppSettings, SocketData } from '@deskthing/types'
+import React, { useEffect, useState } from "react";
+import { DeskThing } from "@deskthing/client";
+import { AppSettings, DEVICE_CLIENT } from "@deskthing/types";
 
 const App: React.FC = () => {
-    const [settings, setSettings] = useState<AppSettings>()
-    const [message, setMessage] = useState<string>()
-    useEffect(() => {
-        const onAppData = async (data: SocketData) => {
-            console.log('Received data from the server!')
-            console.log(data.payload)
-        }
-        
-        
-        const initializeSettings = async () => {
-            const settings = await DeskThing.getSettings()
-            if (settings) {
-                setSettings(settings)
-            }
-            DeskThing.send({ type: 'get', request: 'sampleData' })
-        }
-        const onAppSettings = async (data: SocketData) => {
-            if (data.payload) {
-                setSettings(data.payload)
-            }
-        }
+  const [settings, setSettings] = useState<AppSettings>();
+  
+  useEffect(() => {
+    const initializeSettings = async () => {
+      const settings = await DeskThing.getSettings();
+      if (settings) {
+        setSettings(settings);
+      }
+      DeskThing.send({ type: "get", request: "sampleData" });
+    };
 
-        const onClientData = async (data: SocketData) => {
-            console.log('Received data from the client!')
-            setMessage(data.payload)
+    initializeSettings();
+
+    const removeSettingsListener = DeskThing.on(
+      DEVICE_CLIENT.SETTINGS,
+      (data) => {
+        if (data.payload) {
+          setSettings(data.payload);
         }
-        
-        initializeSettings()
-        
-        const removeDataListener = DeskThing.on('data', onAppData)
-        const removeSettingsListener = DeskThing.on('settings', onAppSettings)
-        const removeClientListener = DeskThing.on('sampleData', onClientData)
+      }
+    );
 
-        return () => {
-            removeDataListener()
-            removeSettingsListener()
-            removeClientListener()
-        }
-    }, [])
+    return () => {
+      removeSettingsListener();
+    };
+  }, []);
 
-    return (
-        <div className="bg-slate-800 gap-2 flex-col w-screen h-screen flex justify-center items-center">
-            <p className="font-bold text-5xl text-white">DeskThing App</p>
-            <div className="text-2xl font-semibold">
-                {settings ? (
-                    <div style={{color: settings?.color?.value as string|| 'white'}}>On the client too!: {message|| 'unknown'}</div>
-                ) : (
-                    <p>Loading Settings</p>
-                )}
-            </div>
-        </div>
+  return (
+    <div className="bg-black gap-2 flex-col w-screen h-screen flex justify-center items-center">
+      <p className="font-bold text-5xl text-white">DeskThing App</p>
+      <div className="text-2xl font-semibold">
+        {settings ? (
+          <div style={{ color: (settings?.color?.value as string) || "white" }}>
+            Current Selected Color: {settings?.color?.value || "unknown"}
+          </div>
+        ) : (
+          <p>Loading Settings</p>
+        )}
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-export default App
+export default App;
